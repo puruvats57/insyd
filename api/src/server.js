bc5334631cb59705c7379af4f28e02d5c626fd6e
prateek <prateekvats963@gmail.com>
2025-11-03 19:15:33 +0530

    add  dotenv library

--- api/src/server.js
+++ api/src/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
+import dotenv from "dotenv";
+dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/insyd_pdc';
